c70332b20c7061872b5fe697fc8bd839876d7c56
sergeybee <raifa@list.ru>
2022-11-14 14:10:53 +0300

    Add implicit and excplicit methods to base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,7 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 class BasePage:
     def __init__(self, driver, url):
         self.driver = driver
@@ -5,3 +9,24 @@ class BasePage:
 
     def open(self):
         self.driver.get(self.url)
+
+    def elemement_is_visible(self, locator, timeout=5):
+        return WebDriverWait(self.driver, locator, timeout).until(EC.visibility_of_element_located(locator))
+
+    def elemements_are_visible(self, locator, timeout=5):
+        return WebDriverWait(self.driver, locator, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+    def elemement_is_present(self, locator, timeout=5):
+        return WebDriverWait(self.driver, locator, timeout).until(EC.presence_of_element_located(locator))
+
+    def elemements_are_present(self, locator, timeout=5):
+        return WebDriverWait(self.driver, locator, timeout).until(EC.presence_of_all_elements_located(locator))
+
+    def elemements_is_not_visible(self, locator, timeout=5):
+        return WebDriverWait(self.driver, locator, timeout).until(EC.invisibility_of_element_located(locator))
+
+    def elemement_is_clickable(self, locator, timeout=5):
+        return WebDriverWait(self.driver, locator, timeout).until(EC.element_to_be_clickable(locator))
+
+    def go_to_element(self, element):
+        self.driver.execute_script("argument[0].ScrollIntoView;")
