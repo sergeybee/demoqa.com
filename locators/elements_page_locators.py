8c2a0589a5412f3ff774d8a8b32c90761a35b811
sergeybee <raifa@list.ru>
2022-11-16 11:18:12 +0300

    Fixed locator

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -5,8 +5,8 @@ class TextBoxPageLocators:
     # Form fields
     FULL_NAME = (By.CSS_SELECTOR, "input[id='userName']")
     EMAIL = (By.CSS_SELECTOR, "input[id='userEmail']")
-    CURRENT_ADDRESS = (By.CSS_SELECTOR, "input[id='currentAddress']")
-    PERMANENT_ADDRESS = (By.CSS_SELECTOR, "input[id='permanentAddress']")
+    CURRENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='currentAddress']")
+    PERMANENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='permanentAddress']")
     SUBMIT = (By.CSS_SELECTOR, "button[id='submit']")
 
     # Created form fields
