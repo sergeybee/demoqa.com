8d843a01788944cc9e17cbc41b6277acf601156f
sergeybee <raifa@list.ru>
2022-11-16 11:13:29 +0300

    Fixed

--- pages/base_page.py
+++ pages/base_page.py
@@ -11,22 +11,22 @@ class BasePage:
         self.driver.get(self.url)
 
     def elemement_is_visible(self, locator, timeout=5):
-        return WebDriverWait(self.driver, locator, timeout).until(EC.visibility_of_element_located(locator))
+        return WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
 
     def elemements_are_visible(self, locator, timeout=5):
-        return WebDriverWait(self.driver, locator, timeout).until(EC.visibility_of_all_elements_located(locator))
+        return WebDriverWait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
 
     def elemement_is_present(self, locator, timeout=5):
-        return WebDriverWait(self.driver, locator, timeout).until(EC.presence_of_element_located(locator))
+        return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located(locator))
 
     def elemements_are_present(self, locator, timeout=5):
-        return WebDriverWait(self.driver, locator, timeout).until(EC.presence_of_all_elements_located(locator))
+        return WebDriverWait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
 
     def elemements_is_not_visible(self, locator, timeout=5):
-        return WebDriverWait(self.driver, locator, timeout).until(EC.invisibility_of_element_located(locator))
+        return WebDriverWait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
 
     def elemement_is_clickable(self, locator, timeout=5):
-        return WebDriverWait(self.driver, locator, timeout).until(EC.element_to_be_clickable(locator))
+        return WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
 
     def go_to_element(self, element):
         self.driver.execute_script("argument[0].ScrollIntoView;")
