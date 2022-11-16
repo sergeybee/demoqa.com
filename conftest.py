6af02a5f65569b47bcccff9340150b37bce3e81c
sergeybee <raifa@list.ru>
2022-11-16 11:17:11 +0300

    Added FireFox webdriver

--- conftest.py
+++ conftest.py
@@ -1,12 +1,15 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service as ChromeService
+# from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FireFoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture(scope='function')
 def driver():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver = webdriver.Firefox(service=FireFoxService(GeckoDriverManager().install()))
     driver.maximize_window()
     yield driver
     driver.quit()
