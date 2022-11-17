3188e0e9625fecdaa9e0c19f2bd17cc77124d556
sergeybee <raifa@list.ru>
2022-11-17 15:12:27 +0300

    Added new page class CheckBox

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,5 +1,9 @@
+import random
+
+from selenium.webdriver.common.by import By
+
 from generator.generator import generated_person
-from locators.elements_page_locators import TextBoxPageLocators
+from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators
 from pages.base_page import BasePage
 
 
@@ -27,3 +31,31 @@ class TextBoxPage(BasePage):
         current_adress = self.element_is_visible(self.locators.CREATED_CURRENT_ADDRESS).text.split(':')[1]
         permanent_adress = self.element_is_visible(self.locators.CREATED_PERMANENT_ADDRESS).text.split(':')[1]
         return full_name, email, current_adress, permanent_adress
+
+
+class CheckBox(BasePage):
+    locators = CheckBoxPageLocators()
+
+    def open_full_list(self):
+        self.element_is_visible(self.locators.EXPAND_BUTTON).click()
+
+    def click_random_checkbox(self):
+        item_list = self.elements_are_visible(self.locators.ITEM_LIST)
+        count = len(item_list)
+
+        while count != 0:
+            item = item_list[(random.randint(1, len(item_list)-1))]
+            if count > 0:
+                self.go_to_element(item)
+                item.click()
+                count -= 1
+            else:
+                break
+
+    def get_checked_checkboxes(self):
+        checked_list = self.elements_are_present(self.locators.CHECKED_ITEMS)
+        data = []
+
+        for box in checked_list:
+            title_item = box.find_element(By.XPATH, self.locators.TITLE_ITEM)
+            print(title_item.text)
