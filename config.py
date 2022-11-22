cc3d9ff64fe4bc718e0745325b2866aa18a25924
sergeybee <raifa@list.ru>
2022-11-22 10:25:33 +0300

    Added new url page

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 URL_PAGE_TEXT_BOX = 'https://demoqa.com/text-box'
 URL_PAGE_CHECK_BOX = 'https://demoqa.com/checkbox'
 URL_PAGE_RADIO_BUTTON = 'https://demoqa.com/radio-button'
+URL_WEB_TABLE_PAGE = 'https://demoqa.com/webtables'
