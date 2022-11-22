b6a0ff3cbe8d910cb639471c9c45cdba76d143f9
sergeybee <raifa@list.ru>
2022-11-22 14:51:46 +0300

    Added new attributes to the Person class

--- data/data.py
+++ data/data.py
@@ -3,7 +3,12 @@ from dataclasses import dataclass
 
 @dataclass
 class Person:
-    full_name: str = None
+    first_name: str = None
+    last_name: str = None
     email: str = None
+    age: int = None
+    salary: int = None
+    department: str = None
+    full_name: str = None
     current_address: str = None
     permanent_address: str = None
