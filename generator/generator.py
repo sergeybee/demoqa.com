b90240de8a240a8baea200291444cc7b3eea7ccb
sergeybee <raifa@list.ru>
2022-11-22 20:51:27 +0300

    Modified

--- generator/generator.py
+++ generator/generator.py
@@ -11,11 +11,11 @@ def generated_person():
     yield Person(
         first_name=fake.first_name(),
         last_name=fake.last_name(),
+        email=fake.email(),
         age=random.randint(18, 65),
         salary=random.randint(400, 1000),
         department=fake.company(),
         full_name=fake.first_name() + " " + fake.last_name() + " " + fake.middle_name(),
-        email=fake.email(),
         current_address=fake.address(),
         permanent_address=fake.address()
     )
