c4633692377355dc991ca77a3bb40bfdf068603e
sergeybee <raifa@list.ru>
2022-11-22 14:53:06 +0300

    Added new attributes to the generated_person method

--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,5 @@
+import random
+
 from data.data import Person
 from faker import Faker
 
@@ -7,8 +9,13 @@ Faker.seed()
 
 def generated_person():
     yield Person(
+        first_name=fake.first_name(),
+        last_name=fake.last_name(),
+        age=random.randint(18, 65),
+        salary=random.randint(400, 1000),
+        department=fake.company(),
         full_name=fake.first_name() + " " + fake.last_name() + " " + fake.middle_name(),
         email=fake.email(),
         current_address=fake.address(),
-        permanent_address=fake.address(),
+        permanent_address=fake.address()
     )
