ed1f108a6e26faf8d9cb60c34851dd3244cf56b5
sergeybee <raifa@list.ru>
2022-11-29 13:25:08 +0300

    Aded new url page

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ URL_PAGE_RADIO_BUTTON = 'https://demoqa.com/radio-button'
 URL_WEB_TABLE_PAGE = 'https://demoqa.com/webtables'
 URL_BUTTONS_PAGE = 'https://demoqa.com/buttons'
 URL_LINKS_PAGE = 'https://demoqa.com/links'
+URL_DOWNLOAD_UPLOAD_PAGE = "https://demoqa.com/upload-download"
