6f60f23536612e8fe7f55247e8a83ca1737cdc78
sergeybee <raifa@list.ru>
2023-02-03 13:58:52 +0300

    UPD

--- locators/form_page_locators.py
+++ locators/form_page_locators.py
@@ -1,23 +1,25 @@
+import random
+
 from selenium.webdriver.common.by import By
 
 
-class RegistrationFormPageLocators:
+class FormPageLocators:
     # Registration Form fields
     FIRST_NAME = (By.CSS_SELECTOR, "input[id='firstName']")
     LAST_NAME = (By.CSS_SELECTOR, "input[id='lastName']")
     USER_EMAIL = (By.CSS_SELECTOR, "input[id='userEmail']")
-    GENDER = (By.CSS_SELECTOR, "input[name='gender']")
+    GENDER = (By.CSS_SELECTOR, f"div[class*='custom-control'] label[for='gender-radio-{random.randint(1, 3)}']")
     USER_PHONE_NUMBER = (By.CSS_SELECTOR, "input[id='userNumber']")
-    DATE_OF_BIRTH = (By.CSS_SELECTOR, "input[id='dateOfBirthInput']")
-    SUBJECTS = (By.CSS_SELECTOR, "input[id='subjectsContainer']")
-    PICTURE_FILE = (By.CSS_SELECTOR, "input[id='uploadPicture']")
+    DATE_OF_BIRTH = (By.CSS_SELECTOR, "div[class='react-datepicker__input-container'] input")
+    SUBJECTS = (By.CSS_SELECTOR, "input[id='subjectsInput']")
+    HOBBIES = (By.CSS_SELECTOR, f"div[class*='custom-control'] label[for='hobbies-checkbox-{random.randint(1, 3)}']")
+    UPLOAD_PICTURE_FILE = (By.XPATH, "//input[@type='file']")
     CURRENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='currentAddress']")
+    STATE_INPUT = (By.CSS_SELECTOR, "input[id='react-select-3-input']")
+    CITY_INPUT = (By.CSS_SELECTOR, "input[id='react-select-4-input']")
     SUBMIT = (By.CSS_SELECTOR, "button[id='submit']")
 
     # Created registration students
-    MODAL_WINDOW = (By.CSS_SELECTOR, "div[class='modal-content']")
-    HEADER_MODAL_WINDOW = (By.CSS_SELECTOR, "div[id='example-modal-sizes-title-lg']")
-
-    CREATED_LIST_DATA = (By.CSS_SELECTOR, "div[class ='modal-body'] table tbody tr")
+    RESULT_FILLS_FORM = (By.XPATH, "//div[@class='table-responsive']//td[2]")
 
 
